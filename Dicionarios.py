70409d15fefb791279a1b3f1583ca631c3d4087c
Jose-Fernandes-de-Souza-Junior <99496702+Jose-Fernandes-de-Souza-Junior@users.noreply.github.com>
2022-06-07 21:49:05 -0400

    Add files via upload

---
+++ Dicionarios.py
@@ -0,0 +1,30 @@
+tradutor = {}
+tradutor = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja" }
+print (type(tradutor))
+print (tradutor)
+
+
+tradutor = {}
+tradutor = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+
+print (tradutor["apple"])
+
+
+tradutor = {}
+tradutor = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+print (tradutor)
+del(tradutor["apple"])
+print (tradutor)
+
+
+tradutor = {}
+tradutor = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+
+print ("apple" in tradutor)
+
+
+tradutor = {}
+tradutor = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+print (tradutor)
+tradutor ["orange"] = "goiaba"
+print(tradutor)
